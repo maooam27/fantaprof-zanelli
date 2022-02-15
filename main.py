686438b3ba3ff7a24ffb06c1565266213a3451a7
maooam27 <52970070+maooam27@users.noreply.github.com>
2022-02-15 20:48:19 +0100

    adding database

---
+++ main.py
@@ -0,0 +1,13 @@
+from Person import *
+
+soragni = Prof("Soragni", 0, "Fisica")
+magliani = Prof("Magliani", 0, "Informatica")
+caroli = Prof("Caroli", 0, "Matematica")
+domenico = Prof("Domenico", 0, "Biologia")
+curcuruto = Prof("Curcuruto", 0, "Chimica")
+fontana = Prof("Fontana", 0, "Inglese")
+viani = Prof("Viani", 0, "Motoria")
+mantovani = Prof("Mantovani", 0, "Arte")
+rosa = Prof("Rosa", 0, "AutoCAD")
+peluso = Prof("Peluso", 0, "Religione")
+roberto = Prof("Roberto", 0, "Italiano")
