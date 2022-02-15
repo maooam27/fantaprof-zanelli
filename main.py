a1b06b3e3cf63ecb75b6c2cab7d2c8a819732cbb
maooam27 <52970070+maooam27@users.noreply.github.com>
2022-02-15 21:11:23 +0100

    initial database use

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from Person import *
+import actions
+import sqlite3
+
+database = sqlite3.connect('database.db')
+cursor = database.cursor()
 
 soragni = Prof("Soragni", 0, "Fisica")
 magliani = Prof("Magliani", 0, "Informatica")
@@ -11,3 +16,9 @@ mantovani = Prof("Mantovani", 0, "Arte")
 rosa = Prof("Rosa", 0, "AutoCAD")
 peluso = Prof("Peluso", 0, "Religione")
 roberto = Prof("Roberto", 0, "Italiano")
+
+database.execute(f"""INSERT INTO Punti (ID, Nome, UltimaAzione, UltimiPunti, Materia, PuntiTotali, DataUltima) 
+VALUES (null, '{soragni.name}', 'azione placeholder', 69, '{soragni.subject}',
+0, datetime('now', 'localtime'))""")
+
+database.commit()
