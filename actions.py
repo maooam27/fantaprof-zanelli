c83cdae2e48a5f0af975aae164c6950096888233
maooam27 <52970070+maooam27@users.noreply.github.com>
2022-02-15 20:59:44 +0100

    bonus malus

---
+++ actions.py
@@ -0,0 +1,138 @@
+# Bonus
+
+def assenza():
+    return(20)
+
+
+def parolaccia():
+    return(30)
+
+
+def scriveAllaLavagna():
+    return(5)
+
+
+def capriolaSullaCattedra():
+    return(200)
+
+
+def maloreInClasse():
+    return(200)
+
+
+def portaInGita():
+    return(50)
+
+
+def inciampa():
+    return(20)
+
+
+def risata():
+    return(10)
+
+
+def mostraCapezzoli():
+    return(500)
+
+
+def relax():
+    return(15)
+
+
+def gergoGiovanile():
+    return(15)
+
+
+def correzioneImmediata():
+    return(15)
+
+
+def defecazioneInClasse():
+    return(500)
+
+
+def complimento():
+    return(10)
+
+
+def nonFunzionaComputer():
+    return(5)
+
+
+def esercitazione():
+    return(20)
+
+
+def vesteMonocromo():
+    return(10)
+
+
+# Malus
+
+def sbagliaNome():
+    return(-10)
+
+
+def perGiornoDopo():
+    return(-15)
+
+
+def nota():
+    return(-30)
+
+
+def pois():
+    return(-5)
+
+
+def supplente():
+    return(-10)
+
+
+def apreRobe():
+    return(-10)
+
+
+def catastrofe():
+    return(-100)
+
+
+def pianto():
+    return(-50)
+
+
+def litiga():
+    return(-50)
+
+
+def ritardo():
+    return(-10)
+
+
+def battuta():
+    return(-15)
+
+
+def dimenticaVerifiche():
+    return(-20)
+
+
+def positivo():
+    return(-30)
+
+
+def insultaPercula():
+    return(-10)
+
+
+def tocca():
+    return(-25)
+
+
+def lavoroGruppo():
+    return(-15)
+
+
+def nonMandaBagno():
+    return(-15)
